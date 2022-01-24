5d915b985342cc67fec51e28357b304ae735b68b
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-01-24 22:34:20 +0700

    unifying functions

---
+++ admin/_utils.php
@@ -0,0 +1,82 @@
+<?php
+function is_array_value_empty($array)
+{
+    $array_key = array_keys($array);
+
+    for ($i = 0; $i < count($array_key); $i++) {
+        if ($array_key[$i] == 'tanggal-bayar') {
+            continue;
+        } else {
+            if (empty($array[$array_key[$i]])) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
+
+function calculate_bayar($jenis, $berat)
+{
+    $result = 0;
+    switch ($jenis) {
+        case "kiloan":
+            $result = 2300 * $berat;
+            break;
+        case "selimut":
+            $result = 3100 * $berat;
+            break;
+        case "karpet":
+            $result = 3700 * $berat;
+            break;
+        default:
+            $result = 0;
+            break;
+    }
+
+    return $result;
+}
+
+function extract_as_table_data($data, $converters = null)
+{
+    foreach ($data as $key => $value) {
+        if ($key === "id") continue;
+
+        $content = isset($converters[$key]) ? $converters[$key]($value) : ucwords(str_replace("_", "", $value));
+?>
+        <td><?= $content ?></td>
+    <?php
+    }
+}
+
+function report_table($mysqli_result, $converters = null)
+{
+    ?>
+    <table class="table table-striped table-borderless mt-3">
+        <thead>
+            <tr>
+                <th> # </th>
+                <?php foreach (mysqli_fetch_fields($mysqli_result) as $field) : if ($field->name === "id") continue; ?>
+                    <th scope="col"> <?= ucwords(str_replace("_", " ", $field->name)) ?> </th>
+                <?php endforeach; ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $no = 0;
+
+            while ($data = mysqli_fetch_assoc($mysqli_result)) {
+                ++$no;
+            ?>
+                <tr>
+                    <th scope="row"><?= $no ?></th>
+                    <?php extract_as_table_data($data, $converters); ?>
+                </tr>
+            <?php
+            }
+            ?>
+        </tbody>
+    </table>
+<?php
+}
+?>
